ce129d4714e1b3598d6fb9b50f85abaa7c3c9764
eilyin <evgen.ilyin@gmail.com>
2020-02-18 21:34:58 +0300

    Initial Commit

---
+++ GORA-CandidacyTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GORA-CandidacyTask
+//
+//  Created by Eugene Ilyin on 18.02.2020.
+//  Copyright © 2020 Eugene Ilyin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
